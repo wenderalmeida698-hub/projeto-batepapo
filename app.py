2cd483ff9e432265fe1cca079fa210ae51635df3
wenderalmeida698-hub <wenderalmeida698@gmail.com>
2026-06-24 16:39:56 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,66 +1,52 @@
-import streamlit as st
 import json
-from datetime import datetime
 from pathlib import Path
+from datetime import datetime
 
-
-st.set_page_config(layout="wide", page_icon="💬")
+import streamlit as st
 
 
 ARQUIVO_MENSAGENS = Path("mensagens.json")
 
+st.set_page_config(layout="wide", page_icon="💬")
 
-def carregar_mensagens():
+def carregar_mensagens(): #Buscar as mensagens no arquivo
     if not ARQUIVO_MENSAGENS.exists():
         return []
-
-
+    
     try:
-        with open(ARQUIVO_MENSAGENS, "r", encoding="utf-8") as f:
-            return json.load(f)
+        with open(ARQUIVO_MENSAGENS, "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+        
     except json.JSONDecodeError:
         return []
 
+def salvar_mensagens(mensagens): #Salvar mensagens no arquivo
+    with open(ARQUIVO_MENSAGENS, "w", encoding="utf-8") as arquivo:
+        json.dump(mensagens, arquivo, indent=4, ensure_ascii=False)
 
-
-def salvar_mensagens(mensagens):
-    with open(ARQUIVO_MENSAGENS, "w", encoding="utf-8") as f:
-        json.dump(mensagens, f, indent=4, ensure_ascii=False)
-
-
-
-def adicionar_mensagem(username, mensagem):
+def adicionar_mensagem(username, mensagem): #Adicionar mensagem a lista de mensagens
     mensagens = carregar_mensagens()
-
 
     horario = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
 
-
-    mensagens.insert(
-        0,
-        {
-            "time": f"({horario})",
-            "username": username,
-            "mensagem": mensagem,
-        },
-    )
-
-
+    mensagens.insert(0,{"time": horario,
+                         "username": username,
+                         "mensagem":mensagem})
+    
     salvar_mensagens(mensagens)
+    
 
-# Interface Principal do site
 
-st.title("🙊Chat entre Usuários")
+st.title("**Chat Público**")
+username = st.sidebar.text_input("**Nome de usuário**", key="username",value="Anônimo")
 
-username = st.sidebar.text_input("Nome de Usuário",key="username",value="Anônimo")
-
+#Área do chat com atualização
 @st.fragment(run_every=3)
 def renderizar_chat():
     mensagens = carregar_mensagens()
     with st.container(border=True, height=500):
-        for msg in mensagens:  
-            st.write(f"**{msg["time"]} - {msg["username"]}**: {msg["mensagem"]}")
-
+        for msg in mensagens:
+            st.write(f"{msg["time"]} — {msg["username"]}: {msg["mensagem"]}")
 renderizar_chat()
 
 entrada_mensagem = st.chat_input("Digite uma mensagem")
@@ -68,4 +54,3 @@ entrada_mensagem = st.chat_input("Digite uma mensagem")
 if entrada_mensagem:
     adicionar_mensagem(username, entrada_mensagem)
     st.rerun()
-
